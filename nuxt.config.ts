687ca1e7193f9b9c17294288ff2284e0a2b3dd5e
georgebrianb <george_brianb@yahoo.com>
2023-11-09 21:10:04 +0100

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
+  hooks: {
+    'components:dirs': (dirs) => {
+      dirs.unshift({
+        path: '~/components/ui',
+        // this is required else Nuxt will autoImport `.ts` file
+        extensions: ['.vue'],
+        // prefix for your components, eg: UiButton
+        prefix: 'Ui',
+        // prevent adding another prefix component by it's path.
+        pathPrefix: false
+      })
+    }
+  }
+})
